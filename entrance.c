30e742cdc1d962ace2b4145d852d85d526388716
Emele Winner orji-Uma <winnerumaemele2007@gmail.com>
2025-04-06 15:48:35 -0700

    null

--- entrance.c
+++ entrance.c
@@ -1,13 +1,26 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
-  char wi = 'a';
-  char *ptr = &wi;
-  printf("%c,%c,%llu", wi, *ptr, (uint64_t)ptr);
-  unsigned x = 'A';
-  ptr = (char *)&x;
-  printf("%u %c %llu", x, *ptr, (uint64_t)ptr);
-  return EXIT_SUCCESS;
+  // null poiters.
+
+  int *ptr = NULL; // Initialize a null pointer
+
+  // Check if the pointer is null before dereferencing
+  if (ptr == NULL) {
+    printf("Pointer is null, cannot dereference it.\n");
+  } else {
+    printf("Pointer value: %d\n", *ptr);
+  }
+
+  // Assign a valid memory address to the pointer
+  int value = 42;
+  ptr = &value;
+
+  // Now the pointer is no longer null
+  if (ptr != NULL) {
+    printf("Pointer is now pointing to value: %d\n", *ptr);
+  }
 }
